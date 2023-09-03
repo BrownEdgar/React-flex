04b6cf25d66eff681c932be642c197f0d6b79cf2
ghostmeta <hwkunique@gmail.com>
2023-09-03 17:58:10 +0400

    First HomeWork

---
+++ src/Users/Vazgen/Components/Users/Users.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Users.scss'
+
+const Users = ({ users }) => {
+  return (
+    <div className="Users">
+        {users.map((user) => (
+          <div className="Users__items" key={user.id}>
+            <h2 className="Users__name">{user.firstName} {user.lastName}</h2>
+            <p className="Users__mail">{user.email}</p>
+            <p className="Users__phone">{user.phone}</p>
+            <img className="Users__img" src={user.image} alt="usersimg" />
+            <p className="Users__age">AGE: {user.age}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Users;

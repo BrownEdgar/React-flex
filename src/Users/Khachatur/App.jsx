7d6c5c832d78a14063b3dcaabcd5db560213c8a7
SpoonAkuzo <bagiyanx@gmail.com>
2023-09-04 22:33:43 +0400

    Khachatur HW

--- src/Users/Khachatur/App.jsx
+++ src/Users/Khachatur/App.jsx
@@ -1,9 +1,21 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Users from './Components/Users/Users'
+import './App.css'
 
 export default function App() {
+	const [users, setUsers] = useState([])
+	useEffect(() => {
+		axios.get("https://dummyjson.com/users")
+		.then((res) => setUsers(res.data.users))
+	})
+
+
+
 	return (
-		<div>
-			<h1>Vazgen App</h1>
+		<div className="App">
+			<h1>Khachatur App</h1>
+			<Users users={users} />
 		</div>
 	)
 }

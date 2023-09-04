42fb6f0a36949474faebcb1273e0bd46b29eb818
hakobyantatev <tatev.hakobyan1997@mail.ru>
2023-09-04 11:31:43 +0400

    My 1 homework

--- src/Users/Tatev/Components/Users.jsx
+++ src/Users/Tatev/Components/Users.jsx
@@ -8,8 +8,10 @@ export default function Users({users}) {
                 return(
                     <div key = {user.id} className="Users__item">
                         <h1 className="Users__title">{user.firstName} {user.lastName}</h1>
-                        <span className ="Users__age">age: {user.age}</span>
-                        <span className ="Users__gen">gender: {user.gender}</span>
+                        <img className="Users__img" src={user.image} alt="" />
+                        <p className ="Users__age">age: {user.age}</p>
+                        <p className ="Users__gen">gender: {user.gender}</p>
+                        <p className ="Users__email">email: {user.email}</p>
                     </div>
                 )
             })

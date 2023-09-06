36a72a2bcfcdf3208fd51478de8e0a8bdc159be4
SpoonAkuzo <bagiyanx@gmail.com>
2023-09-06 21:51:11 +0400

    Khachatur HW 2

--- src/Users/Khachatur/Components/Users.jsx
+++ src/Users/Khachatur/Components/Users.jsx
@@ -1,17 +1,19 @@
 import './Users.scss'
+import classNames from "classnames";
 
-export default function Users({users}) {
+export default function Users({steps}) {
     return (
-        <div className="Users">
-            {users.map((user => {
-                <div className="Users__item">
-                    <h2 className="Users__name">{user.firstname} {user.lastname}</h2>
-                    <p className="Users__mail">{user.email}</p>
-                    <p className="Users__username">{user.username}</p>
-                    <img className="Users__image" src={user.image} alt="userimg" />
-                    <p className="Users__age">Age: {user.age}</p>
-                </div>
-            }))}
+        <div className="Steps">
+            {
+                steps.map((steps) => (
+                    <div className={classNames("Steps__item")} key={steps.id}>
+                        <img src={steps.image} alt="img" />
+                        <h1 className={classNames("Steps__title")}>{steps.title}</h1>
+                        <h2 className={classNames("Steps__desc")}>{steps.desc}</h2>
+                    </div>
+                ))
+            }
         </div>
+            
     )
 }

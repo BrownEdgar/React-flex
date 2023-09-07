7ed8b59be06314c5857be595681fe2c434372844
DarbinyanHayk <darbinyan-22@mail.ru>
2023-09-07 09:16:33 +0400

    Homework second

--- src/Users/Hayk/App.jsx
+++ src/Users/Hayk/App.jsx
@@ -1,18 +1,38 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+import { useState } from "react";
 import Users from "./components/Users";
 
 export default function App() {
-  const [user, setuser] = useState([]);
-  useEffect(() => {
-    axios("https://dummyjson.com/users")
-      .then((res) => res.data.users)
-      .then((user) => setuser(user));
-  });
+  const [information] = useState([
+    {
+      id: 1,
+      image: "./src/Users/Hayk/Images/1.jpg",
+      title: "Process Step One",
+      desc: "I am alone, and feel the charm of existence in this spot, which was created for the bliss...",
+    },
+    {
+      id: 2,
+      image: "./src/Users/Tatev/Images/2.jpg",
+      title: "Process Step Two",
+      desc: "I am alone, and feel the charm of existence in this spot, which was created for the bliss...",
+    },
+    {
+      id: 3,
+      image: "./src/Users/Tatev/Images/3.jpg",
+      title: "Process Step Three",
+      desc: "I am alone, and feel the charm of existence in this spot, which was created for the bliss...",
+    },
+    {
+      id: 4,
+      image: "./src/Users/Tatev/Images/4.jpg",
+      title: "Process Step Four",
+      desc: "I am alone, and feel the charm of existence in this spot, which was created for the bliss...",
+    },
+  ]);
 
   return (
-    <div>
-      <Users users={user} />
+    <div className="container">
+      <Users information={information} />
     </div>
   );
 }

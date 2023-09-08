305e6734a8d1c3eb3c802b31cadff3d9e14dbbe0
Martun <imyaochestvo0@gmail.com>
2023-09-08 13:37:10 +0400

    add my homework

---
+++ src/Users/Martun/les8/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Users from './Users/Users'
+
+export default function App(){
+    return (
+		<div className='container'>
+            <Users />
+		</div>
+		)
+}

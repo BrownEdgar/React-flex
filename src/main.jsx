cf77d653ca75a7a2bd5487272af46c54c687cd62
Levon0117 <lyov.voskanyan02@gmail.com>
2023-09-11 18:14:30 +0400

    lesson3

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./Users/Lyov/Lesson 2/app"
-=======
-import App from "./Users/Edgar/PropTypes/App";
->>>>>>> 809bb80e0062a8c6fd542a0f8f4df958f0111339
+import App from "./Users/Lyov/Lesson3/App"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

e122e171f98058aeb8b167a6d7c09f7a577620dd
DarbinyanHayk <darbinyan-22@mail.ru>
2023-09-12 11:11:53 +0400

    prop-types homework 4

---
+++ src/Users/Hayk/3proptypes/App.jsx
@@ -0,0 +1,27 @@
+const users = {
+  id: 1,
+  firt_name: "Sebastian",
+  last_name: "Rogriges",
+  addres: {
+    street: "Halabyan",
+    house: 54,
+    city: "Yerevan",
+  },
+  age: 25,
+  salary: 560_000,
+  bestFriend: "Jhon",
+};
+
+import { useState } from "react";
+import Users from "./components/Users";
+
+export default function App() {
+  const [information] = useState([users]);
+  console.log(information);
+
+  return (
+    <div className="container">
+      <Users information={information} />
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Lyov/Lesson3/App"
+import App from "./Users";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

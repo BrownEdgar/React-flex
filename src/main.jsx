8d6c5231780a0cf3d5154c1bc60dd973287d0bf1
Levon0117 <lyov.voskanyan02@gmail.com>
2023-09-20 14:25:38 +0400

    formik

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Lyov/Lesson4/App"
-import App from "./Users/Edgar/test/App";
+import App from "./Users/Lyov/FORM/Formik/App"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

28d7e5601cded7abcd8d877f06c7a23a87ad8025
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-09-23 14:41:00 +0400

    useReduser hookexample  added

--- src/Users/Tatev/Lesson14.2 homework/App.jsx
+++ src/Users/Tatev/Lesson14.2 homework/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.scss"
 
 
@@ -7,98 +7,67 @@ export default function App() {
   const [films, setFilms] = useState({
     original: [
       {
-        id:1,
+        id: 1,
         title: "Teminator",
         genre: "Action",
         stock: 6,
         rate: 2.5
       },
       {
-        id:2,
+        id: 2,
         title: "Die Hard",
         genre: "Action",
         stock: 5,
         rate: 2.5
       },
       {
-        id:3,
+        id: 3,
         title: "Get Out",
         genre: "Thriller",
         stock: 8,
         rate: 3.5
       },
       {
-        id:4,
+        id: 4,
         title: "Trip to Italy",
         genre: "Comedy",
         stock: 7,
         rate: 3.5
       },
       {
-        id:5,
+        id: 5,
         title: "Airplane",
         genre: "Comedy",
         stock: 7,
         rate: 3.5
       }
     ],
-    filtered: [
-      {
-        id:1,
-        title: "Teminator",
-        genre: "Action",
-        stock: 6,
-        rate: 2.5
-      },
-      {
-        id:2,
-        title: "Die Hard",
-        genre: "Action",
-        stock: 5,
-        rate: 2.5
-      },
-      {
-        id:3,
-        title: "Get Out",
-        genre: "Thriller",
-        stock: 8,
-        rate: 3.5
-      },
-      {
-        id:4,
-        title: "Trip to Italy",
-        genre: "Comedy",
-        stock: 7,
-        rate: 3.5
-      },
-      {
-        id:5,
-        title: "Airplane",
-        genre: "Comedy",
-        stock: 7,
-        rate: 3.5
-      }
-    ]
+    filtered: []
   })
 
-  const deleteFilmById =(id) => {
+  useEffect(() => {
+    setFilms({ ...films, filtered: films.original })
+  }, [])
+
+
+  const deleteFilmById = (id) => {
     const result = films.filtered.filter(film => film.id !== id)
-    setFilms({original: result, filtered: result})
+    setFilms({ original: result, filtered: result })
   }
 
-  const filterFilms=(e) => {
-    if(e.target.value === ""){
+  const filterFilms = (e) => {
+    if (e.target.value === "") {
       const result = films.original
-      setFilms({...films, filtered: result})
-    } else{
+      setFilms({ ...films, filtered: result })
+    } else {
       const result = films.original.filter(film => film.id === +e.target.value)
-      setFilms({...films, filtered: result})
+      setFilms({ ...films, filtered: result })
     }
   }
 
   return (
     <div className="Container">
-      <input type="text" placeholder="Write id number" onChange={filterFilms}/>
+      <input type="text" placeholder="Write id number" onChange={filterFilms} />
       <table>
         <thead>
           <tr>
@@ -111,19 +80,29 @@ export default function App() {
         </thead>
         <tbody>
           {
-            films.filtered.map(film =>{
-              return(
-                <tr key ={film.id} className="item">
-                  <td>{film.title}</td>
-                  <td>{film.genre}</td>
-                  <td>{film.stock}</td>
-                  <td>{film.rate}</td>
-                  <td>
-                    <button onClick={() => deleteFilmById(film.id)}>Delete</button>
-                  </td>
-                </tr>
-              )
-            })
+            films.filtered.length > 0 ? (
+              <>
+                {
+                  films.filtered.map(film => {
+                    return (
+                      <tr key={film.id} className="item">
+                        <td>{film.title}</td>
+                        <td>{film.genre}</td>
+                        <td>{film.stock}</td>
+                        <td>{film.rate}</td>
+                        <td>
+                          <button onClick={() => deleteFilmById(film.id)}>Delete</button>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </>
+            ) : (
+              <tr className="item">
+                <td colSpan={5}>no fils found</td>
+              </tr>
+            )
           }
         </tbody>
       </table>

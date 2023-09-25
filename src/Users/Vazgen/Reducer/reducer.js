1d7bb59e64e5ff917bc2f3b6f1d3111cb51ccaca
ghostmeta <hwkunique@gmail.com>
2023-09-25 21:24:22 +0400

    React HomeWork

---
+++ src/Users/Vazgen/Reducer/reducer.js
@@ -0,0 +1,63 @@
+export const initialState = {
+  actions: 0,
+  developers: [],
+  posts: [],
+  arr: [154, 42, 1, 87, 695, 36, 2, 10, 39, 9],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-posts":
+      return addPosts(state, action.payload.data);
+    case "add-devs":
+      return addDevs(state, action.payload.inpValue);
+    case "assortment":
+      return assortArray(state);
+    case "replace-idArr":
+      return replaceId(state);
+    case "delete-post":
+      return delPost(state, +action.payload);
+    default:
+      return state;
+  }
+}
+
+function addPosts(state, payload) {
+  const result = [...payload];
+  return {
+    ...state,
+    posts: result,
+    actions: state.actions + 1,
+  };
+}
+
+function addDevs(state, name) {
+  if (state.developers.includes(name.toLowerCase()) || name === "") {
+    return { ...state };
+  } else {
+    return {
+      ...state,
+      developers: [...state.developers, name],
+      actions: state.actions + 1,
+    };
+  }
+}
+
+function assortArray(state) {
+  const result = state.arr.sort(() => Math.random() - 0.5);
+  return { ...state, arr: result, actions: state.actions + 1 };
+}
+
+function replaceId(state) {
+  const result = state.posts.map((post, index) => {
+    return { ...post, id: state.arr[index] };
+  });
+  return { ...state, posts: result };
+}
+
+function delPost(state, id) {
+  const result = state.posts.filter((post) => post.id !== id);
+  return { ...state, posts: result };
+}
+
+export default reducer;

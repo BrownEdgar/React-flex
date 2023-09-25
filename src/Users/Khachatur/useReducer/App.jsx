89283b5cb3424067c6fec5d539497cddc0dec907
SpoonAkuzo <bagiyanx@gmail.com>
2023-09-25 23:29:35 +0400

    Khachatur HW

---
+++ src/Users/Khachatur/useReducer/App.jsx
@@ -0,0 +1,71 @@
+import { useReducer, useState } from 'react'
+import reducer, { initialState } from './reducer'
+import axios from 'axios'
+import { AiFillCloseSquare } from 'react-icons/ai'
+import "./App.scss"
+
+
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [disableButton, setDisableButton] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+
+  const showPosts = () => { 
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    .then((res) => res.data)
+    .then((data) => {
+      dispatch({ type: "show-posts", payload: { data: data } });
+    });
+ }
+
+ const addDev = () => {
+  dispatch({ type: "add-dev", payload: { inputValue: inputValue }});
+  setInputValue("")
+ }
+
+  return (
+    <div className='Container'>
+      <div className='Container__items'>
+        <p>Actions: {state.actions}</p>
+        {state.developers.length > 0 ?  (
+          <p>Devs: {" "}
+          {state.developers.map((dev, index) => {
+            return <span key={index}>{dev}</span>
+          })}</p>
+        ) : null}
+        <button onClick={showPosts} disabled={disableButton}>Show users</button>
+        <input
+          className="Container__addtext"
+          type="text"
+          placeholder="Enter name"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addDev}>Add dev</button>
+        <button onClick={() => dispatch({ type: "sort"})}>Sort Array</button>
+        <button onClick={() => dispatch({ type: "change-id"})}>Change ID</button>
+        <div className="item"></div>
+      </div>
+      <div className='Container__posts'>
+        {state.posts.length > 0 ? 
+        state.posts.map((post) => {
+          return(
+            <div className="Container__posts-item" key={post.id}>
+              <h1 className='Container__posts-id'>{post.id}</h1>
+              <h3 className='Container__posts-title'>{post.title}</h3>
+              <button className="Container__posts-icon"
+              onClick={() => dispatch({ type: "delete-post", payload: `${post.id}`})}>
+              <AiFillCloseSquare/>
+              </button>
+              <p className="Container__post-body">{post.body}</p>
+            </div>
+          )
+        })
+        : null}
+      </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Ararat/Hooks/App";
+import App from "./Users/Khachatur/useReducer/App";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

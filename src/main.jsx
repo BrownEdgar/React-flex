ce3621d97a2211e292d3d3ae843432ad99580159
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-10-17 14:45:48 +0400

    added local storage example with add to card button

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Tatev/Classwork/Lesson 22/App"
+import App from "./App"
 import "./index.css";
-import store from "./Users/Tatev/Classwork/Lesson 22/app/store";
-import { Provider } from "react-redux";
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-<Provider store ={ store}>
-  <App />
-</Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)

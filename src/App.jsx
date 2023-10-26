18d80d58e63e3cf892c65bddd248660ad0bf3434
ghostmeta <hwkunique@gmail.com>
2023-10-26 18:02:41 +0400

    Changes Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import { Layouts, Shop, Card } from "./Components";
+import AutoSlide from "./Components/AutoSlide/AutoSlide";
 import "./App.scss";
 
 export default function App() {
-  const [lengthOfItems, setLengthOfItems] = useState(0);
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layouts itemLength={lengthOfItems} />}>
-        <Route
-          index
-          element={
-            <Shop itemLength={lengthOfItems} setLength={setLengthOfItems} />
-          }
-        />
-        <Route path="/card" element={<Card />} />
-      </Route>
-    )
-  );
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <h2>AutoPlay Slider</h2>
+      <AutoSlide />
     </div>
   );
 }
--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Card } from "./Card/Card";
-export { default as Shop } from "./Shop/Shop";
-export { default as Layouts } from "./Layouts/Layouts";
-export { default as Navbar } from "./Navbar/Navbar";
